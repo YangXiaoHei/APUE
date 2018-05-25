d7a6e8a0bc66e0dfe12d0b6a43ad3d6b2268f717
yangxiaohei <yangxiaohei321123@163.com>
2018-05-25 16:08:03 +0800

    APUE
    第五章总结

---
+++ Chapter_05/review/setbuf_imp.c
@@ -0,0 +1,17 @@
+
+void yh_setbuf(FILE *fp, char *buf) {
+    if (buf == NULL) {
+        setvbuf(fp, NULL, _IONBF, 1);
+    } else {
+        setvbuf(fp, )
+    }
+}
+
+
+
+int main(int argc, char *argv) {
+    
+    
+    
+    
+}
