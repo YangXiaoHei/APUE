f17dec30352a1599a14b165714d58f93f94b82f1
yangxiaohei <yangxiaohei321123@163.com>
2018-05-25 17:17:59 +0800

    APUE
    第 5 章练习题

--- Chapter_05/review/setbuf_imp.c
+++ Chapter_05/review/setbuf_imp.c
@@ -1,17 +1,72 @@
 
-void yh_setbuf(FILE *fp, char *buf) {
-    if (buf == NULL) {
-        setvbuf(fp, NULL, _IONBF, 1);
-    } else {
-        setvbuf(fp, )
-    }
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdarg.h>
+
+/*
+ 该输出函数不会改变标准 I/O 库的缓冲区，
+ 并且每次调用都对应一次系统调用 write
+ */
+size_t __not_use_stdio_printf(const char *format, ...) {
+    if (format == NULL)  return 0;
+    va_list vl;
+    va_start(vl, format);
+    char buf[1024] = { 0 };
+    vsnprintf(buf, sizeof(buf), format, vl);
+    size_t len = strlen(buf), written = 0;
+    if ((written = write(STDOUT_FILENO, buf, len)) != len)
+        return written;
+    va_end(vl);
+    return len;
 }
 
+#ifdef printf_nb
+#error what a fuck ??
+#else
+#define printf_nb(_format_, ...) \
+__not_use_stdio_printf(_format_, ##__VA_ARGS__)
+#endif
 
+/*
+ 打印标准 I/O 缓冲区信息
+ */
+void print_stdio_buffer_info_nb() {
+    
+    printf_nb("---------------------------\n");
+    printf_nb("stdio buffer type : ");
+    if      (stdout->_flags & _IONBF)   printf_nb("no buffer");
+    else if (stdout->_flags & _IOLBF)   printf_nb("line buffer");
+    else                                printf_nb("full buffer");
+    printf_nb("\n");
+    printf_nb("stdio buffer size : %ld B\n", stdout->_bf._size);
+    printf_nb("stdio buffer first bytes : %c\n",
+              stdout->_bf._base != NULL ?
+              (*stdout->_bf._base == 0 ? '?' : *stdout->_bf._base )
+              : ' ');
+    printf_nb("---------------------------\n");
+}
 
-int main(int argc, char *argv) {
+#ifdef print_stdio_buffer_info_nb
+#error what a fuck ??
+#else
+#define info_nb \
+print_stdio_buffer_info_nb();
+#endif
+
+void yh_setbuf(FILE *fp, char *buf) {
+    setvbuf(fp, buf, (buf ? _IOFBF : _IONBF), (buf ? BUFSIZ : 0));
+}
+
+int main(int argc, char *argv[]) {
     
+    info_nb;
     
+    printf_nb("invoke yh_setbuf(stdout, NULL)\n");
+    yh_setbuf(stdout, NULL);
     
+    info_nb;
     
+    _exit(0);
 }
